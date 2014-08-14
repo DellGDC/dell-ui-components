17eba21ed01e979940c625b3229a3cea0499f09c
Bo Lora <bo_lora@dell.com>
2014-08-13 23:44:43 -0500

    fixed issue with build and message data

--- app/shared/masthead/masthead.js
+++ app/shared/masthead/masthead.js
@@ -1,11 +1,15 @@
-angular.module('dellUi').directive('dellUiSiteMasthead', function($rootScope) {
+angular.module('dellUi').directive('dellUiSiteMasthead', function($rootScope, utils) {
 	return {
 		restrict: 'A',
 		replace: true,
 		scope: true,
 		templateUrl: 'app/shared/masthead/masthead.html',
 		link: function(scope, element, attrs, fn) {
-			scope.version = $rootScope.messages.version;
+			
+			utils.getJSON("app/data/messages.json", function(data){
+		        scope.version = data.version;
+		    });
+
 		}
 	};
 });
