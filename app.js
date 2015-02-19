033a503ec356f31451b650ce7d9e539e37b96dfb
Bo Lora <bo_lora@dell.com>
2015-02-19 10:21:50 -0600

    testing new deploy script #deploy-review

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* globals s */
-angular.module('demo', ['ui.bootstrap', 'ui.utils', 'ui.router', 'ngAnimate', 'dellUiComponents']);
+angular.module('demo', ['ui.bootstrap', 'ui.utils', 'ui.router', 'ngAnimate', 'dellUiComponents', 'ui.grid']);
 
 angular.module('demo').config(function ($stateProvider, $urlRouterProvider) {
 
