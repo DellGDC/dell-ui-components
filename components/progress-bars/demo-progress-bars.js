6e888912f687977f1fd9c97753862f43542549a6
Clint Batte <clint_batte@dell.com>
2015-02-25 11:52:11 -0600

    Updated Progress bars... changed next step color from grey to dell hover blue color.

--- components/progress-bars/demo-progress-bars.js
+++ components/progress-bars/demo-progress-bars.js
@@ -1,6 +1,30 @@
 
-angular.module('demo').controller('progressBarsCtrl',function($scope,$rootScope) {
+angular.module('demo').controller('progressBarsCtrl',function($scope,$timeout,$rootScope) {
 	//this is for functionality related to demo code
+
+    $scope.fakeAnimationValue = 0;
+    $scope.fakeAnimation = function() {
+        $scope.fakeAnimationId = $timeout(function(){
+            if($scope.fakeAnimationValue < 100) {
+                $scope.fakeAnimationValue = $scope.fakeAnimationValue + 1;
+                $scope.fakeAnimationSteps = Math.round($scope.fakeAnimationValue/20);
+                console.log($scope.fakeAnimationValue,$scope.fakeAnimationSteps);
+                $scope.stripeAnimate = "active";
+                $scope.fakeAnimation();
+            }
+        },_.random(100,500));
+    };
+
+    $scope.pauseFakeAnimation = function() {
+        $timeout.cancel($scope.fakeAnimationId);
+        $scope.fakeAnimationId = undefined;
+        $scope.stripeAnimate = "";
+    };
+    $scope.resetFakeAnimation = function() {
+        $scope.fakeAnimationValue = 0;
+        $scope.fakeAnimation();
+        $scope.stripeAnimate = "active";
+    };
 
 });
 
