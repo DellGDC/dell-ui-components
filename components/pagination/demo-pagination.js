494a3c37ca3ca114c2bc8b6913b4251dacf345c9
Clint Batte <clint_batte@dell.com>
2015-03-06 08:54:03 -0600

    check in progress work for pagination

--- components/pagination/demo-pagination.js
+++ components/pagination/demo-pagination.js
@@ -1,6 +1,11 @@
 
 angular.module('demo').controller('paginationCtrl',function($scope,$rootScope) {
 	//this is for functionality related to demo code
+    $('.pagination').jqPagination({
+        paged: function(page) {
+            // do something with the page variable
+        }
+    });
 
 });
 
