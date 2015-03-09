3f7c84a387b7c6ed0c41089a922d0178eb9468cc
Clint Batte <clint_batte@dell.com>
2015-03-09 09:19:03 -0500

    in progress check-in

--- components/pagination/demo-pagination.js
+++ components/pagination/demo-pagination.js
@@ -7,6 +7,26 @@ angular.module('demo').controller('paginationCtrl',function($scope,$rootScope) {
         }
     });
 
+    //
+    //$scope.loadingButtonInit = function () {
+    //    $('#loading-example-btn').click(function () {
+    //        var btn = $(this);
+    //        btn.button('loading');
+    //        window.setTimeout(function () {
+    //            btn.button('reset');
+    //        }, 2000);
+    //    });
+    //};
+
+    $scope.loadingButtonAction = function () {
+        event.preventDefault();
+        var btn = $(event.currentTarget);
+        btn.button('loading');
+        $timeout(function () {
+            btn.button('reset');
+        }, 2000);
+    };
+
 });
 
 angular.module('demo').controller('paginationPLayDemoCtrl',function($scope,$rootScope,$sce) {
