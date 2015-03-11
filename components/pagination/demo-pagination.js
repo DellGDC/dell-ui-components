2d188d0fee1538753ff3a8f90f01f84cabcdca44
Clint Batte <clint_batte@dell.com>
2015-03-11 08:55:54 -0500

    update to divider vertical, change color to #cccccc
    added jpage.js, in progress check in for pagination

--- components/pagination/demo-pagination.js
+++ components/pagination/demo-pagination.js
@@ -15,17 +15,40 @@ angular.module('demo').controller('paginationCtrl',function($scope, $rootScope, 
             window.setTimeout(function () {
                 btn.button('reset');
             }, 2000);
+
+            //var lazyLoad = document.getElementById("loading-example-btn");
+            //lazyLoad.addEventListener("click", firstSet);
+            //
+            //function firstSet(){
+            //    document.getElementById("results").innerHTML += "1 of 20";
+            //}
         });
     };
 
-    $scope.loadingButtonAction = function () {
-        event.preventDefault();
-        var btn = $(event.currentTarget);
-        btn.button('loading');
-        $timeout(function () {
-            btn.button('reset');
-        }, 2000);
-    };
+    $(function() {
+        /* initiate plugin */
+        $("div.holder").jPages({
+            containerID : "itemContainer",
+            callback    : function( pages, items ){
+                $("#legend1").html("Page " + pages.current + " of " + pages.count);
+                $("#legend2").html(items.range.start + " - " + items.range.end + " of " + items.count);
+            }
+        });
+    });
+
+    //var lazyLoad = document.getElementById("loading-example-btn");
+    //lazyLoad.addEventListener("click", firstClick);
+    //lazyLoad.addEventListener("click", secondClick);
+    //
+    //function firstClick() {
+    //    document.getElementById("results").innerHTML += "1 of 20"
+    //}
+    //
+    //function secondClick() {
+    //    document.getElementById("results").innerHTML += "21 of 40"
+    //}
+
+
 
 });
 
