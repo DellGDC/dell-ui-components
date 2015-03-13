a7578e41a5fdcceff57c8c00b95cc7a2d88c0513
Clint Batte <clint_batte@dell.com>
2015-03-13 11:41:34 -0500

    checking in pagination files

--- components/pagination/demo-pagination.js
+++ components/pagination/demo-pagination.js
@@ -3,50 +3,25 @@ angular.module('demo').controller('paginationCtrl',function($scope, $rootScope, 
 	//this is for functionality related to demo code
     $('.pagination').jqPagination({
         paged: function(page) {
-            // do something with the page variable
         }
     });
 
 
-    $scope.loadingButtonInit = function () {
-        $('#loading-example-btn').click(function () {
-            var btn = $(this);
-            btn.button('loading');
-            window.setTimeout(function () {
-                btn.button('reset');
-            }, 2000);
 
-            //var lazyLoad = document.getElementById("loading-example-btn");
-            //lazyLoad.addEventListener("click", firstSet);
-            //
-            //function firstSet(){
-            //    document.getElementById("results").innerHTML += "1 of 20";
-            //}
-        });
-    };
+    $(document).ready(function () {
 
-    $(function() {
-        /* initiate plugin */
-        $("div.holder").jPages({
-            containerID : "itemContainer",
-            callback    : function( pages, items ){
-                $("#legend1").html("Page " + pages.current + " of " + pages.count);
-                $("#legend2").html(items.range.start + " - " + items.range.end + " of " + items.count);
+        $('.news-pagination li').slice(5).hide();
+
+        $('#loadmore').jqPagination({
+            max_page: Math.ceil($('.news-pagination li').length / 5),
+            paged: function (page) {
+                $('.news-pagination li').hide();
+                $('.news-pagination li').slice((page - 1) * 5, (page * 5)).fadeIn('slow');
             }
         });
     });
 
-    //var lazyLoad = document.getElementById("loading-example-btn");
-    //lazyLoad.addEventListener("click", firstClick);
-    //lazyLoad.addEventListener("click", secondClick);
-    //
-    //function firstClick() {
-    //    document.getElementById("results").innerHTML += "1 of 20"
-    //}
-    //
-    //function secondClick() {
-    //    document.getElementById("results").innerHTML += "21 of 40"
-    //}
+
 
 
 
