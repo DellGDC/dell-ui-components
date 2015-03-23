9497f6e28dd65fbc882b870f06a81ac75aeea17c
Bo Lora <bo_lora@dell.com>
2015-03-23 12:16:19 -0500

    fixed carousels

--- components/carousel/demo-carousel.js
+++ components/carousel/demo-carousel.js
@@ -3,37 +3,13 @@ angular.module('demo').controller('carouselCtrl',function($scope,$rootScope) {
 	//this is for functionality related to demo code
 
 
-})
 
-    .directive('carousel', function($timeout) {
-        // Runs during compile
-        return {
-            restrict: "C",
-            link: function($scope, iElm, iAttrs, controller) {
-                $timeout(function() {
-                    $(iElm).carousel();
-                });
-            }
-        };
-    });
+});
 
 
-//
-//angular.module('ui.bootstrap.carousel', ['ui.bootstrap.transition'])
-//    .controller('CarouselController', function($scope, $timeout, $transition, $q) {})
-//    .directive('carousel', function($timeout) {
-//        // Runs during compile
-//        return {
-//            restrict: "C",
-//            link: function($scope, iElm, iAttrs, controller) {
-//                $timeout(function() {
-//                    $(iElm).carousel();
-//                });
-//            }
-//        };
-//    });
 
 angular.module('demo').controller('carouselPLayDemoCtrl',function($scope,$rootScope,$sce) {
 	//this is for functionality related to play demo code
 
 });
+
