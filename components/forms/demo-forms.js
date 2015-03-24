28e637a8c1aef9f4fd0ddac40f4a6c23848112f6
Clint Batte <clint_batte@dell.com>
2015-03-24 09:18:06 -0500

    working on tree view

--- components/forms/demo-forms.js
+++ components/forms/demo-forms.js
@@ -1,7 +1,20 @@
 
 angular.module('demo').controller('formsCtrl',function($scope,$rootScope) {
 	//this is for functionality related to demo code
-
+    $(function () {
+        $('.tree li:has(ul)').addClass('parent_li').find(' > input').attr('title', 'Collapse this branch');
+        $('.tree li.parent_li > input').on('click', function (e) {
+            var children = $(this).parent('li.parent_li').find(' > ul > li');
+            if (children.is(":visible")) {
+                children.hide('fast');
+                //$(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
+            } else {
+                children.show('fast');
+                //$(this).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
+            }
+            e.stopPropagation();
+        });
+    });
 });
 
 angular.module('demo').controller('formsPLayDemoCtrl',function($scope,$rootScope,$sce) {
