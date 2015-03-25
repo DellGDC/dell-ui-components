be8b52e255f154420793f61f711a57eaa8a603b4
Bo Lora <bo_lora@dell.com>
2015-03-25 09:22:49 -0500

    fixed list-tree

--- components/forms/demo-forms.js
+++ components/forms/demo-forms.js
@@ -1,20 +1,6 @@
 
 angular.module('demo').controller('formsCtrl',function($scope,$rootScope) {
 
-    $(function () {
-        $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
-        $('.tree li.parent_li > span').on('click', function (e) {
-            var children = $(this).parent('li.parent_li').find(' > ul > li');
-            if (children.is(":visible")) {
-                children.hide('fast');
-                $(this).attr('title', 'Expand this branch');
-            } else {
-                children.show('fast');
-                $(this).attr('title', 'Collapse this branch');
-            }
-            e.stopPropagation();
-        });
-    });
 
     //
     //$(function () {
--- components/forms/forms.js
+++ components/forms/forms.js
@@ -1,7 +1,23 @@
 /**
  * Created by Clint_Batte on 3/24/2015.
  */
-//angular.module('dellUiComponents')
+angular.module('dellUiComponents')
+
+.directive('listTree', function () {
+    return {
+        restrict: 'C',
+        link: function ($scope, $element, $attr) {
+            $element.find('.checkbox input').on('click', function(){
+                if($(this).is(':checked')) {    
+                    $(this).parent().addClass('open');
+                } else {
+                    $(this).parent().removeClass('open');
+                }
+            });
+        }
+    };
+});
+
 //
 //.directive('tree', function(){
 //    $(function () {
