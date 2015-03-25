9e7c0cb377bbbff93148e78705951f7badb695bc
Clint Batte <clint_batte@dell.com>
2015-03-25 10:30:28 -0500

    Resolved TreeVIew with JavaScript per Bo

--- components/forms/forms.js
+++ components/forms/forms.js
@@ -8,7 +8,7 @@ angular.module('dellUiComponents')
         restrict: 'C',
         link: function ($scope, $element, $attr) {
             $element.find('.checkbox input').on('click', function(){
-                if($(this).is(':checked')) {    
+                if($(this).is(':checked')) {
                     $(this).parent().addClass('open');
                 } else {
                     $(this).parent().removeClass('open');
@@ -17,40 +17,3 @@ angular.module('dellUiComponents')
         }
     };
 });
-
-//
-//.directive('tree', function(){
-//    $(function () {
-//        $('.tree li:has(ul)').addClass('parent_li').find(' > input').attr('title', 'Collapse this branch');
-//        $('.tree li.parent_li > input').on('click', function (e) {
-//            var children = $(this).parent('li.parent_li').find(' > ul > li');
-//            if (children.is(":visible")) {
-//                children.hide('fast');
-//                //$(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
-//            } else {
-//                children.show('fast');
-//                //$(this).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
-//            }
-//            e.stopPropagation();
-//        })
-//    })
-//});
-//
-
-//
-//
-//.directive('tree', function($timeout){
-//    $(function () {
-//        $('.tree li:has(ul)').addClass('parent_li').find(' > input').attr('title', 'Collapse this branch');
-//        $('.tree li.parent_li > input').on('click', function (e) {
-//            var children = $(this).parent('li.parent_li').find(' > ul > li');
-//            if (children.is(":visible")) {
-//                children.hide('fast');
-//                //$(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
-//            } else {
-//                children.show('fast');
-//                //$(this).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
-//            }
-//            e.stopPropagation();
-//        })
-//    })
