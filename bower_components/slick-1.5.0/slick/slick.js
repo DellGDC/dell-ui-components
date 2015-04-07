e069f08294f5bafb7cb38d853c19e5e9fd19d150
Clint Batte <clint_batte@dell.com>
2015-04-07 16:51:42 -0500

    working on film-strip javaScript... not solved yet

--- bower_components/slick-1.5.0/slick/slick.js
+++ bower_components/slick-1.5.0/slick/slick.js
@@ -1,24 +1,24 @@
 /*
-     _ _      _       _
+ _ _      _       _
  ___| (_) ___| | __  (_)___
-/ __| | |/ __| |/ /  | / __|
-\__ \ | | (__|   < _ | \__ \
-|___/_|_|\___|_|\_(_)/ |___/
-                   |__/
+ / __| | |/ __| |/ /  | / __|
+ \__ \ | | (__|   < _ | \__ \
+ |___/_|_|\___|_|\_(_)/ |___/
+ |__/
 
  Version: 1.5.0
-  Author: Ken Wheeler
+ Author: Ken Wheeler
  Website: http://kenwheeler.github.io
-    Docs: http://kenwheeler.github.io/slick
-    Repo: http://github.com/kenwheeler/slick
-  Issues: http://github.com/kenwheeler/slick/issues
+ Docs: http://kenwheeler.github.io/slick
+ Repo: http://github.com/kenwheeler/slick
+ Issues: http://github.com/kenwheeler/slick/issues
 
  */
 /* global window, document, define, jQuery, setInterval, clearInterval */
 (function(factory) {
     'use strict';
     if (typeof define === 'function' && define.amd) {
-        define(['../../jquery/jquery'], factory);
+        define(['jquery'], factory);
     } else if (typeof exports !== 'undefined') {
         module.exports = factory(require('jquery'));
     } else {
@@ -52,18 +52,11 @@
                 centerMode: false,
                 centerPadding: '50px',
                 cssEase: 'ease',
-                //customPaging: function(slider, i) {
-                //    return '';
-                //},
-                //dots: true,
-                //dotsClass: 'carousel-indicators',
                 customPaging: function(slider, i) {
                     return '<button type="button" data-role="none">' + (i + 1) + '</button>';
                 },
                 dots: false,
                 dotsClass: 'slick-dots',
-                //need a way to set the side as active
-                active:'active',
                 draggable: true,
                 easing: 'linear',
                 edgeFriction: 0.35,
@@ -159,7 +152,7 @@
                         _.breakpoints.push(responsiveSettings[
                             breakpoint].breakpoint);
                         _.breakpointSettings[responsiveSettings[
-                                breakpoint].breakpoint] =
+                            breakpoint].breakpoint] =
                             responsiveSettings[breakpoint].settings;
                     }
                 }
@@ -304,11 +297,11 @@
                         now = Math.ceil(now);
                         if (_.options.vertical === false) {
                             animProps[_.animType] = 'translate(' +
-                                now + 'px, 0px)';
+                            now + 'px, 0px)';
                             _.$slideTrack.css(animProps);
                         } else {
                             animProps[_.animType] = 'translate(0px,' +
-                                now + 'px)';
+                            now + 'px)';
                             _.$slideTrack.css(animProps);
                         }
                     },
@@ -473,7 +466,7 @@
             _.$dots = $(dotString).appendTo(
                 _.options.appendDots);
 
-            _.$dots.find('li').first().addClass('active').attr('aria-hidden', 'false');
+            _.$dots.find('li').first().addClass('slick-active').attr('aria-hidden', 'false');
 
         }
 
@@ -580,7 +573,7 @@
         }
 
         if (_.originalSettings.responsive && _.originalSettings
-            .responsive.length > -1 && _.originalSettings.responsive !== null) {
+                .responsive.length > -1 && _.originalSettings.responsive !== null) {
 
             targetBreakpoint = null;
 
@@ -671,7 +664,7 @@
 
             case 'index':
                 var index = event.data.index === 0 ? 0 :
-                    event.data.index || $(event.target).parent().index() * _.options.slidesToScroll;
+                event.data.index || $(event.target).parent().index() * _.options.slidesToScroll;
 
                 _.slideHandler(_.checkNavigable(index), false, dontAnimate);
                 break;
@@ -802,7 +795,7 @@
         }
 
         if (_.$slides) {
-            _.$slides.removeClass('slick-slide active slick-center slick-visible')
+            _.$slides.removeClass('slick-slide slick-active slick-center slick-visible')
                 .attr('aria-hidden', 'true')
                 .removeAttr('data-slick-index')
                 .css({
@@ -1393,13 +1386,13 @@
         if (imgCount > 0) {
             targetImage = $('img[data-lazy]', _.$slider).first();
             targetImage.attr('src', targetImage.attr('data-lazy')).removeClass('slick-loading').load(function() {
-                    targetImage.removeAttr('data-lazy');
-                    _.progressiveLazyLoad();
+                targetImage.removeAttr('data-lazy');
+                _.progressiveLazyLoad();
 
-                    if (_.options.adaptiveHeight === true) {
-                        _.setPosition();
-                    }
-                })
+                if (_.options.adaptiveHeight === true) {
+                    _.setPosition();
+                }
+            })
                 .error(function() {
                     targetImage.removeAttr('data-lazy');
                     _.progressiveLazyLoad();
@@ -1724,7 +1717,7 @@
         var _ = this,
             centerOffset, allSlides, indexOffset, remainder;
 
-        _.$slider.find('.slick-slide').removeClass('active').attr('aria-hidden', 'true').removeClass('slick-center');
+        _.$slider.find('.slick-slide').removeClass('slick-active').attr('aria-hidden', 'true').removeClass('slick-center');
         allSlides = _.$slider.find('.slick-slide');
 
         if (_.options.centerMode === true) {
@@ -1734,10 +1727,10 @@
             if (_.options.infinite === true) {
 
                 if (index >= centerOffset && index <= (_.slideCount - 1) - centerOffset) {
-                    _.$slides.slice(index - centerOffset, index + centerOffset + 1).addClass('active').attr('aria-hidden', 'false');
+                    _.$slides.slice(index - centerOffset, index + centerOffset + 1).addClass('slick-active').attr('aria-hidden', 'false');
                 } else {
                     indexOffset = _.options.slidesToShow + index;
-                    allSlides.slice(indexOffset - centerOffset + 1, indexOffset + centerOffset + 2).addClass('active').attr('aria-hidden', 'false');
+                    allSlides.slice(indexOffset - centerOffset + 1, indexOffset + centerOffset + 2).addClass('slick-active').attr('aria-hidden', 'false');
                 }
 
                 if (index === 0) {
@@ -1753,16 +1746,16 @@
         } else {
 
             if (index >= 0 && index <= (_.slideCount - _.options.slidesToShow)) {
-                _.$slides.slice(index, index + _.options.slidesToShow).addClass('active').attr('aria-hidden', 'false');
+                _.$slides.slice(index, index + _.options.slidesToShow).addClass('slick-active').attr('aria-hidden', 'false');
             } else if (allSlides.length <= _.options.slidesToShow) {
-                allSlides.addClass('active').attr('aria-hidden', 'false');
+                allSlides.addClass('slick-active').attr('aria-hidden', 'false');
             } else {
                 remainder = _.slideCount % _.options.slidesToShow;
                 indexOffset = _.options.infinite === true ? _.options.slidesToShow + index : index;
                 if (_.options.slidesToShow == _.options.slidesToScroll && (_.slideCount - index) < _.options.slidesToShow) {
-                    allSlides.slice(indexOffset - (_.options.slidesToShow - remainder), indexOffset + remainder).addClass('active').attr('aria-hidden', 'false');
+                    allSlides.slice(indexOffset - (_.options.slidesToShow - remainder), indexOffset + remainder).addClass('slick-active').attr('aria-hidden', 'false');
                 } else {
-                    allSlides.slice(indexOffset, indexOffset + _.options.slidesToShow).addClass('active').attr('aria-hidden', 'false');
+                    allSlides.slice(indexOffset, indexOffset + _.options.slidesToShow).addClass('slick-active').attr('aria-hidden', 'false');
                 }
             }
 
@@ -1796,7 +1789,7 @@
                 }
 
                 for (i = _.slideCount; i > (_.slideCount -
-                        infiniteCount); i -= 1) {
+                infiniteCount); i -= 1) {
                     slideIndex = i - 1;
                     $(_.$slides[slideIndex]).clone(true).attr('id', '')
                         .attr('data-slick-index', slideIndex - _.slideCount)
@@ -1841,8 +1834,8 @@
         if (!index) index = 0;
 
         if (_.slideCount <= _.options.slidesToShow) {
-            _.$slider.find('.slick-slide').removeClass('active').attr('aria-hidden', 'true');
-            _.$slides.eq(index).addClass('active').attr("aria-hidden", "false");
+            _.$slider.find('.slick-slide').removeClass('slick-active').attr('aria-hidden', 'true');
+            _.$slides.eq(index).addClass('slick-active').attr("aria-hidden", "false");
             if (_.options.centerMode === true) {
                 _.$slider.find('.slick-slide').removeClass('slick-center');
                 _.$slides.eq(index).addClass('slick-center');
@@ -2229,7 +2222,7 @@
         if (_.$nextArrow && (typeof _.options.nextArrow !== 'object')) {
             _.$nextArrow.remove();
         }
-        _.$slides.removeClass('slick-slide active slick-visible').attr("aria-hidden", "true").css('width', '');
+        _.$slides.removeClass('slick-slide slick-active slick-visible').attr("aria-hidden", "true").css('width', '');
 
     };
 
@@ -2271,8 +2264,8 @@
 
         if (_.$dots !== null) {
 
-            _.$dots.find('li').removeClass('active').attr("aria-hidden", "true");
-            _.$dots.find('li').eq(Math.floor(_.currentSlide / _.options.slidesToScroll)).addClass('active').attr("aria-hidden", "false");
+            _.$dots.find('li').removeClass('slick-active').attr("aria-hidden", "true");
+            _.$dots.find('li').eq(Math.floor(_.currentSlide / _.options.slidesToScroll)).addClass('slick-active').attr("aria-hidden", "false");
 
         }
 
--- components/carousel/carousel.js
+++ components/carousel/carousel.js
@@ -48,13 +48,13 @@ angular.module('dellUiComponents')
                 infinite: false,
                 speed: 300,
                 slidesToShow: 4,
-                slidesToScroll: 4,
+                slidesToScroll:1,
                 responsive: [
                     {
                         breakpoint: 1024,
                         settings: {
-                            slidesToShow: 5,
-                            slidesToScroll: 3,
+                            slidesToShow: 4,
+                            slidesToScroll: 1,
                             infinite: true,
                             dots: true
                         }
@@ -63,7 +63,7 @@ angular.module('dellUiComponents')
                         breakpoint: 600,
                         settings: {
                             slidesToShow: 2,
-                            slidesToScroll: 2
+                            slidesToScroll: 1
                         }
                     },
                     {
