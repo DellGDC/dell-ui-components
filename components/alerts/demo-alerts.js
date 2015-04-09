bd86b0d2ae7cf09e08ed39cf4e143f490c1ae777
Bo Lora <bolora@hotmail.com>
2015-04-09 11:39:29 -0500

    separated directive function into a dell-ui-component module directive

--- components/alerts/demo-alerts.js
+++ components/alerts/demo-alerts.js
@@ -2,23 +2,8 @@
 angular.module('demo').controller('alertsCtrl',function($scope,$rootScope) {
 	//this is for functionality related to demo code
 
-})
-.directive('alertCollapsible', function () {
-	return {
-		restrict: 'C',
-		link: function ($scope, $element, $attrs) {
-			//toggle x
-			$element.find('.close').on('click',function(){
-				$(event.currentTarget).parent().addClass('collapsed');
-			});
-			$element.find('> .show-collapsed').on('click',function(){
-				$(event.currentTarget).parent().removeClass('collapsed');
-								console.log("hello",$(event.currentTarget).parent());
+});
 
-			});
-		}
-	};
-})
 
 angular.module('demo').controller('alertsPLayDemoCtrl',function($scope,$rootScope,$sce) {
 	//this is for functionality related to play demo code
