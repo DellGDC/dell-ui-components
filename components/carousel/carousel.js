420caa964b039c66b10fdc661b90b63932d20554
Bo Lora <bolora@hotmail.com>
2015-04-14 16:11:08 -0500

    fixed carousel

--- components/carousel/carousel.js
+++ components/carousel/carousel.js
@@ -6,6 +6,16 @@ angular.module('ui.bootstrap.carousel', ['ui.bootstrap.transition'])
 
 
 angular.module('dellUiComponents')
+/*.directive('carousel', function($timeout){
+    // Runs during compile
+    return {
+        restrict: 'C',
+        link: function($scope, $element, iAttrs, controller) {
+            //$element.carousel();
+        }
+    };
+})*/
+
 .directive('carouselFilmstrip', function($timeout){
     // Runs during compile
     // requires bower_components/slick-1.5.0/slick/slick.js which is bundled in dell-ui-components.js
@@ -47,6 +57,82 @@ angular.module('dellUiComponents')
                     // instead of a settings object
                 ]
             });
+        }
+    };
+})
+/*
+.directive('responsiveFilmstrip', function($timeout){
+    // Runs during compile
+    return {
+        restrict: 'AC',
+        link: function($scope, $element, iAttrs, controller) {
+            $('.responsive-filmstrip').slick({
+                dots: true,
+                infinite: false,
+                speed: 300,
+                slidesToShow: 4,
+                slidesToScroll:1,
+                responsive: [
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 4,
+                            slidesToScroll: 1,
+                            infinite: true,
+                            dots: true
+                        }
+                    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 1
+                        }
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
+                    }
+                    // You can unslick at a given breakpoint now by adding:
+                    // settings: "unslick"
+                    // instead of a settings object
+                ]
+            });
+        }
+    };
+})
+
+//.directive('variableWidth', function($timeout){
+//    // Runs during compile
+//    return {
+//        restrict: 'AC',
+//        link: function($scope, $element, iAttrs, controller) {
+//            $('.variable-width').slick({
+//                dots: true,
+//                infinite: true,
+//                speed: 300,
+//                slidesToShow: 1,
+//                centerMode: true,
+//                variableWidth: true
+//            });
+//        }
+//    };
+//})
+
+*/
+
+
+.directive('slide', function($timeout){
+    return {
+        restrict: 'A',
+        link: function($scope, $element, $attr, controller) {
+            $element.on('click', function(event) {
+                event.preventDefault();
+            });
+            $element.carousel($attr.slide);
         }
     };
 });
