843e3d3612dddbf281af9b186d08f7e30a8572f2
Keith Kritselis <keith_kritselis@dellteam.com>
2015-04-15 15:59:48 -0500

    updates to tables and content teasers

---
+++ components/tables/directive-data-table.js
@@ -0,0 +1,23 @@
+console.log("hello woo");
+
+angular.module('dellUiComponents').directive('table', function(){
+	console.log("woohoo");
+    // Runs during compile
+    return {
+        // name: '',
+        // priority: 1,
+        // terminal: true,
+        scope: {}, // {} = isolate, true = child, false/undefined = no change
+        // controller: function($scope, $element, $attrs, $transclude) {},
+        // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+        restrict: 'C', // E = Element, A = Attribute, C = Class, M = Comment
+        // template: '',
+        // replace: true,
+        // transclude: true,
+        // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+        link: function($scope, $element, $attrs, controller) {
+
+        		$element.bootstrapTable();	
+        }
+    };
+});
