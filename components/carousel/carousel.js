1d895b870578ceb466506561071240aed7c31ff0
Clint Batte <clint_batte@dell.com>
2015-05-01 16:55:58 -0500

    carousel check-in, adding matchHeight code

--- components/carousel/carousel.js
+++ components/carousel/carousel.js
@@ -61,23 +61,20 @@ angular.module('dellUiComponents')
     };
 })
 
-    //.directive('divHeightEqualize', function($timeout){
-    //    // Runs during compile
-    //    // requires bower_components/slick-1.5.0/slick/slick.js which is bundled in dell-ui-components.js
-    //    return {
-    //        restrict: 'C',
-    //        link: function($scope, $element, iAttrs, controller ) {
-    //            //
-    //
-    //            var maxHeight = 0;
-    //            $($element).each(function(){
-    //                if ($(this).height() > maxHeight) { maxHeight = $(this).height(); }
-    //            });
-    //
-    //            $($element).height(maxHeight);
-    //        }
-    //    };
-    //})
+    .directive('divHeightEqualize', function($timeout){
+        // Runs during compile
+        // requires bower_components/slick-1.5.0/slick/slick.js which is bundled in dell-ui-components.js
+        return {
+            restrict: 'C',
+            link: function($scope, $element, iAttrs, controller ) {
+                //
+
+                $(function() {
+                    $('.div-height-equalize').matchHeight();
+                });
+            }
+        };
+    })
 
 
 
