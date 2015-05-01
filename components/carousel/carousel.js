975c2f81e5c8bb9ff2ddb91e72122a6e2c2d16c5
Clint Batte <clint_batte@dell.com>
2015-05-01 09:01:22 -0500

    changes to carousel... working on adaptive carousel

--- components/carousel/carousel.js
+++ components/carousel/carousel.js
@@ -60,72 +60,25 @@ angular.module('dellUiComponents')
         }
     };
 })
-/*
-.directive('responsiveFilmstrip', function($timeout){
+
+.directive('mosaicCarousel', function($timeout){
     // Runs during compile
+    // requires bower_components/slick-1.5.0/slick/slick.js which is bundled in dell-ui-components.js
     return {
-        restrict: 'AC',
+        restrict: 'C',
         link: function($scope, $element, iAttrs, controller) {
-            $('.responsive-filmstrip').slick({
-                dots: true,
-                infinite: false,
-                speed: 300,
-                slidesToShow: 4,
-                slidesToScroll:1,
-                responsive: [
-                    {
-                        breakpoint: 1024,
-                        settings: {
-                            slidesToShow: 4,
-                            slidesToScroll: 1,
-                            infinite: true,
-                            dots: true
-                        }
-                    },
-                    {
-                        breakpoint: 600,
-                        settings: {
-                            slidesToShow: 2,
-                            slidesToScroll: 1
-                        }
-                    },
-                    {
-                        breakpoint: 480,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1
-                        }
-                    }
-                    // You can unslick at a given breakpoint now by adding:
-                    // settings: "unslick"
-                    // instead of a settings object
-                ]
-            });
+
+            $('.carousel').carousel('pause');
+
+
+
         }
     };
 })
 
-//.directive('variableWidth', function($timeout){
-//    // Runs during compile
-//    return {
-//        restrict: 'AC',
-//        link: function($scope, $element, iAttrs, controller) {
-//            $('.variable-width').slick({
-//                dots: true,
-//                infinite: true,
-//                speed: 300,
-//                slidesToShow: 1,
-//                centerMode: true,
-//                variableWidth: true
-//            });
-//        }
-//    };
-//})
-
-*/
 
 
-.directive('slide', function($timeout){
+    .directive('slide', function($timeout){
     return {
         restrict: 'A',
         link: function($scope, $element, $attr, controller) {
