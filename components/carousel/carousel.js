19bcdf1c57a2d80a3dd681b0823bbb36561b677f
Clint Batte <clint_batte@dell.com>
2015-05-05 13:49:16 -0500

    Created filmstrip with content for SSI

--- components/carousel/carousel.js
+++ components/carousel/carousel.js
@@ -6,106 +6,110 @@ angular.module('ui.bootstrap.carousel', ['ui.bootstrap.transition'])
 
 
 angular.module('dellUiComponents')
-.directive('carousel', function($timeout){
-    // Runs during compile
-    return {
-        restrict: 'C',
-        link: function($scope, $element, iAttrs, controller) {
-            $element.carousel();
-        }
-    };
-})
+    .directive('carousel', function($timeout){
+        // Runs during compile
+        return {
+            restrict: 'C',
+            link: function($scope, $element, iAttrs, controller) {
+                $element.carousel();
+            }
+        };
+    })
 
-.directive('carouselFilmstrip', function($timeout){
-    // Runs during compile
-    // requires bower_components/slick-1.5.0/slick/slick.js which is bundled in dell-ui-components.js
-    return {
-        restrict: 'C',
-        link: function($scope, $element, iAttrs, controller) {
-            $($element).find(".carousel-inner").slick({
-                dots: true,
-                infinite: false,
-                speed: 300,
-                slidesToShow: 4,
-                slidesToScroll:1,
-                responsive: [
-                    {
-                        breakpoint: 1024,
-                        settings: {
-                            slidesToShow: 4,
-                            slidesToScroll: 1,
-                            infinite: true,
-                            dots: true
+    .directive('carouselFilmstrip', function($timeout){
+        // Runs during compile
+        // requires bower_components/slick-1.5.0/slick/slick.js which is bundled in dell-ui-components.js
+        return {
+            restrict: 'C',
+            link: function($scope, $element, iAttrs, controller) {
+                $($element).find(".carousel-inner").slick({
+                    dots: true,
+                    infinite: false,
+                    speed: 300,
+                    slidesToShow: 4,
+                    slidesToScroll:1,
+                    responsive: [
+                        {
+                            breakpoint: 1024,
+                            settings: {
+                                slidesToShow: 4,
+                                slidesToScroll: 1,
+                                infinite: true,
+                                dots: true
+                            }
+                        },
+                        {
+                            breakpoint: 600,
+                            settings: {
+                                slidesToShow: 2,
+                                slidesToScroll: 1
+                            }
+                        },
+                        {
+                            breakpoint: 480,
+                            settings: {
+                                slidesToShow: 1,
+                                slidesToScroll: 1
+                            }
                         }
-                    },
-                    {
-                        breakpoint: 600,
-                        settings: {
-                            slidesToShow: 2,
-                            slidesToScroll: 1
-                        }
-                    },
-                    {
-                        breakpoint: 480,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1
-                        }
-                    }
-                    // You can unslick at a given breakpoint now by adding:
-                    // settings: "unslick"
-                    // instead of a settings object
-                ]
-            });
-        }
-    };
-})
+                        // You can unslick at a given breakpoint now by adding:
+                        // settings: "unslick"
+                        // instead of a settings object
+                    ]
+                });
+            }
+        };
+    })
 
+    .directive('carouselFilmstripArrowOnly', function($timeout){
+        // Runs during compile
+        // requires bower_components/slick-1.5.0/slick/slick.js which is bundled in dell-ui-components.js
+        return {
+            restrict: 'C',
+            link: function($scope, $element, iAttrs, controller) {
+                $($element).find(".carousel-inner").slick({
+                    dots: false,
+                    infinite: false,
+                    speed: 300,
+                    slidesToShow: 4,
+                    slidesToScroll:1,
+                    responsive: [
+                        {
+                            breakpoint: 1024,
+                            settings: {
+                                slidesToShow: 4,
+                                slidesToScroll: 1,
+                                infinite: false,
+                                dots: false
+                            }
+                        },
+                        {
+                            breakpoint: 600,
+                            settings: {
+                                slidesToShow: 2,
+                                slidesToScroll: 1,
+                                infinite: false,
+                                dots: false
+                            }
+                        },
+                        {
+                            breakpoint: 480,
+                            settings: {
+                                slidesToShow: 1,
+                                slidesToScroll: 1,
+                                infinite: false,
+                                dots: false
+                            }
+                        }
+                        // You can unslick at a given breakpoint now by adding:
+                        // settings: "unslick"
+                        // instead of a settings object
+                    ]
+                });
+            }
+        };
+    })
 
-.directive('carouselArrowsFilmstrip', function($timeout){
-    // Runs during compile
-    // requires bower_components/slick-1.5.0/slick/slick.js which is bundled in dell-ui-components.js
-    return {
-        restrict: 'C',
-        link: function($scope, $element, iAttrs, controller) {
-            $($element).find(".carousel-inner").slick({
-                dots: true,
-                infinite: false,
-                speed: 300,
-                slidesToShow: 4,
-                slidesToScroll:1,
-                responsive: [
-                    {
-                        breakpoint: 1024,
-                        settings: {
-                            slidesToShow: 4,
-                            slidesToScroll: 1,
-                            infinite: true,
-                            dots: true
-                        }
-                    },
-                    {
-                        breakpoint: 600,
-                        settings: {
-                            slidesToShow: 2,
-                            slidesToScroll: 1
-                        }
-                    },
-                    {
-                        breakpoint: 480,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1
-                        }
-                    }
-                    // You can unslick at a given breakpoint now by adding:
-                    // settings: "unslick"
-                    // instead of a settings object
-                ]
-            });
-        }
-    };
-})
 
     .directive('divHeightEqualize', function($timeout){
         // Runs during compile
@@ -123,10 +127,7 @@ angular.module('dellUiComponents')
     })
 
 
-
-
-
-    .directive('slide', function($timeout){
+.directive('slide', function($timeout){
     return {
         restrict: 'A',
         link: function($scope, $element, $attr, controller) {
@@ -136,4 +137,5 @@ angular.module('dellUiComponents')
             $element.carousel($attr.slide);
         }
     };
+
 });
