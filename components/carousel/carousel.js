5cb6c55b0548191130c6326a78f0d20d819991a3
Clint Batte <clint_batte@dell.com>
2015-05-05 16:12:19 -0500

    merge attempt

--- components/carousel/carousel.js
+++ components/carousel/carousel.js
@@ -6,123 +6,125 @@ angular.module('ui.bootstrap.carousel', ['ui.bootstrap.transition'])
 
 
 angular.module('dellUiComponents')
-.directive('carousel', function($timeout){
-    // Runs during compile
-    return {
-        restrict: 'C',
-        link: function($scope, $element, iAttrs, controller) {
-            $element.carousel();
-        }
-    };
-})
+    .directive('carousel', function($timeout){
+        // Runs during compile
+        return {
+            restrict: 'C',
+            link: function($scope, $element, iAttrs, controller) {
+                $element.carousel();
+            }
+        };
+    })
 
-.directive('carouselFilmstrip', function($timeout){
-    // Runs during compile
-    // requires bower_components/slick-1.5.0/slick/slick.js which is bundled in dell-ui-components.js
-    return {
-        restrict: 'C',
-        link: function($scope, $element, iAttrs, controller) {
-            $($element).find(".carousel-inner").slick({
-                dots: true,
-                infinite: false,
-                speed: 300,
-                slidesToShow: 4,
-                slidesToScroll:1,
-                responsive: [
-                    {
-                        breakpoint: 1024,
-                        settings: {
-                            slidesToShow: 4,
-                            slidesToScroll: 1,
-                            infinite: true,
-                            dots: true
+    .directive('carouselFilmstrip', function($timeout){
+        // Runs during compile
+        // requires bower_components/slick-1.5.0/slick/slick.js which is bundled in dell-ui-components.js
+        return {
+            restrict: 'C',
+            link: function($scope, $element, iAttrs, controller) {
+                $($element).find(".carousel-inner").slick({
+                    dots: true,
+                    infinite: false,
+                    speed: 300,
+                    slidesToShow: 4,
+                    slidesToScroll:1,
+                    responsive: [
+                        {
+                            breakpoint: 1024,
+                            settings: {
+                                slidesToShow: 4,
+                                slidesToScroll: 1,
+                                infinite: true,
+                                dots: true
+                            }
+                        },
+                        {
+                            breakpoint: 600,
+                            settings: {
+                                slidesToShow: 2,
+                                slidesToScroll: 1
+                            }
+                        },
+                        {
+                            breakpoint: 480,
+                            settings: {
+                                slidesToShow: 1,
+                                slidesToScroll: 1
+                            }
                         }
-                    },
-                    {
-                        breakpoint: 600,
-                        settings: {
-                            slidesToShow: 2,
-                            slidesToScroll: 1
-                        }
-                    },
-                    {
-                        breakpoint: 480,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1
-                        }
-                    }
-                    // You can unslick at a given breakpoint now by adding:
-                    // settings: "unslick"
-                    // instead of a settings object
-                ]
-            });
-        }
-    };
-})
-/*
-.directive('responsiveFilmstrip', function($timeout){
-    // Runs during compile
-    return {
-        restrict: 'AC',
-        link: function($scope, $element, iAttrs, controller) {
-            $('.responsive-filmstrip').slick({
-                dots: true,
-                infinite: false,
-                speed: 300,
-                slidesToShow: 4,
-                slidesToScroll:1,
-                responsive: [
-                    {
-                        breakpoint: 1024,
-                        settings: {
-                            slidesToShow: 4,
-                            slidesToScroll: 1,
-                            infinite: true,
-                            dots: true
-                        }
-                    },
-                    {
-                        breakpoint: 600,
-                        settings: {
-                            slidesToShow: 2,
-                            slidesToScroll: 1
-                        }
-                    },
-                    {
-                        breakpoint: 480,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1
-                        }
-                    }
-                    // You can unslick at a given breakpoint now by adding:
-                    // settings: "unslick"
-                    // instead of a settings object
-                ]
-            });
-        }
-    };
-})
+                        // You can unslick at a given breakpoint now by adding:
+                        // settings: "unslick"
+                        // instead of a settings object
+                    ]
+                });
+            }
+        };
+    })
 
-//.directive('variableWidth', function($timeout){
-//    // Runs during compile
-//    return {
-//        restrict: 'AC',
-//        link: function($scope, $element, iAttrs, controller) {
-//            $('.variable-width').slick({
-//                dots: true,
-//                infinite: true,
-//                speed: 300,
-//                slidesToShow: 1,
-//                centerMode: true,
-//                variableWidth: true
-//            });
-//        }
-//    };
-//})
+    .directive('carouselFilmstripArrowOnly', function($timeout){
+        // Runs during compile
+        // requires bower_components/slick-1.5.0/slick/slick.js which is bundled in dell-ui-components.js
+        return {
+            restrict: 'C',
+            link: function($scope, $element, iAttrs, controller) {
+                $($element).find(".carousel-inner").slick({
+                    dots: false,
+                    infinite: false,
+                    speed: 300,
+                    slidesToShow: 4,
+                    slidesToScroll:1,
+                    responsive: [
+                        {
+                            breakpoint: 1024,
+                            settings: {
+                                slidesToShow: 4,
+                                slidesToScroll: 1,
+                                infinite: false,
+                                dots: false
+                            }
+                        },
+                        {
+                            breakpoint: 600,
+                            settings: {
+                                slidesToShow: 2,
+                                slidesToScroll: 1,
+                                infinite: false,
+                                dots: false
+                            }
+                        },
+                        {
+                            breakpoint: 480,
+                            settings: {
+                                slidesToShow: 1,
+                                slidesToScroll: 1,
+                                infinite: false,
+                                dots: false
+                            }
+                        }
+                        // You can unslick at a given breakpoint now by adding:
+                        // settings: "unslick"
+                        // instead of a settings object
+                    ]
+                });
+            }
+        };
+    })
 
-*/
+
+    .directive('divHeightEqualize', function($timeout){
+        // Runs during compile
+        // requires bower_components/slick-1.5.0/slick/slick.js which is bundled in dell-ui-components.js
+        return {
+            restrict: 'C',
+            link: function($scope, $element, iAttrs, controller ) {
+                //
+
+                $(function() {
+                    $('.div-height-equalize').matchHeight();
+                });
+            }
+        };
+    })
 
 
 .directive('slide', function($timeout){
@@ -135,4 +137,5 @@ angular.module('dellUiComponents')
             $element.carousel($attr.slide);
         }
     };
+
 });
