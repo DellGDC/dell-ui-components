ac06e8a4670d73f8c84e072357073f50d75fedc1
Clint Batte <clint_batte@dell.com>
2015-05-07 13:57:24 -0500

    fixed +/- show hide

--- components/pagination/demo-pagination.js
+++ components/pagination/demo-pagination.js
@@ -6,25 +6,6 @@ angular.module('demo').controller('paginationCtrl',function($scope, $rootScope, 
         }
     });
 
-
-    //
-    //$(document).ready(function () {
-    //
-    //    $('.news-pagination li').slice(5).hide();
-    //
-    //    $('#loadmore').jqPagination({
-    //        max_page: Math.ceil($('.news-pagination li').length / 5),
-    //        paged: function (page) {
-    //            $('.news-pagination li').hide();
-    //            $('.news-pagination li').slice((page - 1) * 5, (page * 5)).fadeIn('slow');
-    //        }
-    //    });
-    //});
-
-
-
-
-
 });
 
 angular.module('demo').controller('paginationPLayDemoCtrl',function($scope,$rootScope,$sce) {
