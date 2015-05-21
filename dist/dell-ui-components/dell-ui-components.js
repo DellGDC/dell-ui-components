7f8056bdfdbbd0049127eb0fdf6d286a169a5f45
Clint Batte <clint_batte@dell.com>
2015-05-21 11:36:25 -0500

    grunt build, update carousel slider dots padding left right

--- dist/dell-ui-components/dell-ui-components.js
+++ dist/dell-ui-components/dell-ui-components.js
@@ -7468,4 +7468,63 @@ angular.module('dellUiComponents').directive('tapToLoad', function () {
       });
     }
   };
+}).directive('pagination', function () {
+  return {
+    restrict: 'C',
+    link: function ($scope, $element, attrs) {
+      $('.pagination').jqPagination({
+        paged: function (page) {
+        }
+      });
+    }
+  };
 });
+/**
+ * Created by Clint_Batte on 5/18/2015.
+ */
+angular.module('dellUiComponents').directive('interactiveProgressBar', [
+  '$timeout',
+  function ($timeout) {
+    return {
+      restrict: 'C',
+      link: function ($scope, $element, iAttrs, controller) {
+        $scope.fakeAnimationValue = 0;
+        $scope.fakeAnimation = function () {
+          $scope.fakeAnimationId = $timeout(function () {
+            if ($scope.fakeAnimationValue < 100) {
+              $scope.fakeAnimationValue = $scope.fakeAnimationValue + 1;
+              $scope.fakeAnimationSteps = Math.round($scope.fakeAnimationValue / 20);
+              console.log($scope.fakeAnimationValue, $scope.fakeAnimationSteps);
+              $scope.stripeAnimate = 'active';
+              $scope.fakeAnimation();
+            }
+          }, _.random(100, 500));
+        };
+        $scope.pauseFakeAnimation = function () {
+          $timeout.cancel($scope.fakeAnimationId);
+          $scope.fakeAnimationId = undefined;
+          $scope.stripeAnimate = '';
+        };
+        console.log('hello timeout');
+        $scope.resetFakeAnimation = function () {
+          $scope.fakeAnimationValue = 0;
+          $scope.fakeAnimation();
+          $scope.stripeAnimate = 'active';
+        };
+      }
+    };
+  }
+]);
+/**
+ * Created by Clint_Batte on 5/18/2015.
+ */
+angular.module('dellUiComponents').directive('ratingsAndReviews', [
+  '$timeout',
+  function ($timeout) {
+    return {
+      restrict: 'C',
+      link: function ($scope, $element, iAttrs, controller) {
+      }
+    };
+  }
+]);
