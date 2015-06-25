7fd10cd84b1f6fd0ad589f6538d0cd3e2de7e24f
Clint Batte <clint_batte@dell.com>
2015-06-25 16:35:16 -0500

    adding in JS for simple table responsive

---
+++ components/directives/directive-responsive-table.js
@@ -0,0 +1,21 @@
+angular.module('dellUiComponents').directive('responsiveTable', function(){
+    // Runs during compile
+    return {
+        restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+        link: function($scope, $element, $attrs, controller) {
+            var selector = $attrs.responsiveTable;
+            if(selector) {
+                $timeout(function(){
+                    $(selector).rtResponsiveTables();
+                },300);
+            } else {
+                console.error('responsive-table usage error. Must include css selector to identify objects to equalize. Example: responsive-table=".classname"');
+            }
+        }
+    };
+});
+
+
+//$("table").rtResponsiveTables({
+//    containerBreakPoint: 300
+//});
