2bc92fca46fd5d5d63641eaf61ecd9df9a89c2e1
Clint Batte <clint_batte@dell.com>
2015-06-29 10:28:08 -0500

    check-in... directive not working yet

--- components/tables/tables.js
+++ components/tables/tables.js
@@ -1,9 +1,14 @@
 /**
  * Created by Clint_Batte on 6/26/2015.
  */
-
-
-Eve.scope('#responsive-simple-table', function() {
-    $("table").rtResponsiveTables();
-
-});
+//
+//
+//Eve.scope('#responsive-simple-table', function() {
+//
+//    this.listen('table', function() {
+//        $("table").rtResponsiveTables();
+//    });
+//
+//
+//console.log('hello. this is firing')
+//});
