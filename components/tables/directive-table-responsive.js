4aad02287c3477bbed7be845e34a028474e4242a
Clint Batte <clint_batte@dell.com>
2015-06-30 11:22:53 -0500

    Updated

--- components/tables/directive-table-responsive.js
+++ components/tables/directive-table-responsive.js
@@ -1,11 +1,15 @@
-console.log("hello there");
+
 angular.module('dellUiComponents').directive('tableResponsive', function($timeout){
     // Runs during compile
     return {
         restrict: 'AC', // E = Element, A = Attribute, C = Class, M = Comment
         link: function($scope, $element, $attrs, controller) {
-        	console.log("hello");
-            $element.rtResponsiveTables();
+            $element.rtResponsiveTables({
+                containerBreakPoint: 300
+            });
         }
     };
 });
+
+
+
