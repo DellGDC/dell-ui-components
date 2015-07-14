b5148510609f6754828e1de1793bba736dc008c9
Clint Batte <clint_batte@dell.com>
2015-07-14 13:03:36 -0500

    adding tablesorter files

---
+++ components/tables/tables.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Clint_Batte on 7/14/2015.
+ */
+
+
+/* ======================================================================================
+ * Dell-UI-Components: tables.js
+ * http://www.delldesignlibrary.com/components/tables/
+ * ======================================================================================
+ * Copyright 2015 Dell, Inc.
+ * Licensed under MIT (https://github.com/DellGDC/dell-ui-components/blob/master/LICENSE)
+ * ======================================================================================
+ */
+
+
+//
+//(function($){
+//    Eve.scope('.contact-drawer', function() {
+//        this.listen('.contact-drawer-cta', 'click', function(e) {
+//            $(e.currentTarget).parent().toggleClass('open');
+//        });
+//    });
+//})(jQuery);
+//
+
+$(function(){
+    $("#myTable").tablesorter();
+});
