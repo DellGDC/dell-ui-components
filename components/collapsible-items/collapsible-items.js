c96b7de74c8217f0bd8bbe8300ea0b7d0a35b2bb
Clint Batte <clint_batte@dell.com>
2015-08-12 09:54:56 -0500

    Check-in progress

--- components/collapsible-items/collapsible-items.js
+++ components/collapsible-items/collapsible-items.js
@@ -10,72 +10,75 @@ angular.module('dellUiComponents')
             restrict: 'C',
             link: function($scope, $element, iAttrs, controller ) {
 
-                    $element.find('.content-gallery-show-more').on('click',function(e){
-                        e.preventDefault();
-                        var $this = $(this);
-                        $this.toggleClass('content-gallery-show-more');
-                        if($this.hasClass('content-gallery-show-more')){
-                            $this.text('See More');
-                        } else {
-                            $this.text('See Less');
-                        }
+                $element.find('.content-gallery-show-more').on('click',function(e){
+                    e.preventDefault();
 
+                    var parentLi = $(e.currentTarget).parents('li')[0],
+                        allListItems = $element.find('li'),
+                        rowWidth= 0,
+                        rowMaxWidth = Math.abs($('.container').css('width').replace(/px/,'')),
+                        targetFound,
+                        done,
+                        //------------
+                        $this = $(this),
+                        content;
 
-                        var parentLi = $(e.currentTarget).parents('li')[0],
-                            allListItems = $element.find('li'),
-                            rowWidth= 0,
-                            rowMaxWidth = Math.abs($('.container').css('width').replace(/px/,'')),
-                            targetFound,
-                            done,
-                            content;
+                    $element.find('.open').removeClass('open');
+                    $element.find('li.details-container').remove();
+                    $(parentLi).addClass('open');
 
+                    //--------------------------------------------
+
+                    $this.toggleClass('content-gallery-show-more');
+                    if ($(this).text() === 'Show More') {
+                        $(this).text('Show Less');
+                        $element.find('.open').addClass('open');
+
+                    } else {
+                        $(this).text('Show More');
                         $element.find('.open').removeClass('open');
                         $element.find('li.details-container').remove();
-                        $(parentLi).addClass('open');
+                    }
 
-                        _.each(allListItems,function(i){
-                            if(!done) {
-                                var itemWidth = Math.abs(($(i).css('width')).replace(/px/, ''));
-                                if (!targetFound) {
-                                    targetFound = $(i).hasClass('open');
-                                    content = $(i).find('.content-gallery-details').html();
-                                }
-                                rowWidth = rowWidth + itemWidth;
-                                console.log("item width", itemWidth, rowWidth, rowMaxWidth);
 
-                                if (rowWidth >= rowMaxWidth) {
-                                    if (targetFound) {
-                                        console.log("Found target and inserting!!!");
-                                        $(i).after('<li class="col-xs-12 details-container"><span class="close"><i class="icon-ui-close"></i></span>'+content+'</li>');
-                                        $('.details-container .close').on('click',function(e){
-                                            e.preventDefault();
-                                            $element.find('.open').removeClass('open');
-                                            $element.find('li.details-container').remove();
-                                        });
-                                        $('.details-container').on('click',function(e){
-                                            e.preventDefault();
-                                            $element.find('.open').removeClass('open');
-                                            $element.find('li.details-container').remove();
-                                        });
-                                        //$('.content-gallery-show-more').on('click',function(e){
-                                        //    e.preventDefault();
-                                        //    $element.find('.open').removeClass('open');
-                                        //    $element.find('li.details-container').remove();
-                                        //});
-                                        done = true;
-                                    } else {
+                    _.each(allListItems,function(i){
+                        if(!done) {
+                            var itemWidth = Math.abs(($(i).css('width')).replace(/px/, ''));
+                            if (!targetFound) {
+                                targetFound = $(i).hasClass('open');
+                                content = $(i).find('.content-gallery-details').html();
+                            }
+                            rowWidth = rowWidth + itemWidth;
+                            console.log("item width", itemWidth, rowWidth, rowMaxWidth);
 
-                                        rowWidth = 0;
-                                    }
-
+                            if (rowWidth >= rowMaxWidth) {
+                                if (targetFound) {
+                                    console.log("Found target and inserting!!!");
+                                    $(i).after('<li class="col-xs-12 details-container"><span class="close"><i class="icon-ui-close"></i></span>'+content+'</li>');
+                                    $('.details-container .close').on('click',function(e){
+                                        e.preventDefault();
+                                        $element.find('.open').removeClass('open');
+                                        $element.find('li.details-container').remove();
+                                    });
+                                    $('.details-container').on('click',function(e){
+                                        e.preventDefault();
+                                        $element.find('.open').removeClass('open');
+                                        $element.find('li.details-container').remove();
+                                    });
+                                    $('.content-gallery-show-more').on('click',function(e){
+                                        e.preventDefault();
+                                        //$element.find('Show Less').text('Show More');
+                                        $element.find('.open').removeClass('open');
+                                    });
+                                    done = true;
+                                } else {
+                                    rowWidth = 0;
                                 }
                             }
-                        });
-
-
+                        }
                     });
-                    console.log('++++++++++++++++++++ It Fired',$scope, $element, iAttrs, controller );
-
+                });
+                console.log('++++++++++++++++++++ It Fired',$scope, $element, iAttrs, controller );
             }
         };
     });
