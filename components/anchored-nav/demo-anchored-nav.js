7871ed37a5ab25b5cc122ae14d15ff26d5535d78
Clint Batte <clint_batte@dell.com>
2015-08-24 15:36:58 -0500

    created component branch... initial set-up/check-in

---
+++ components/anchored-nav/demo-anchored-nav.js
@@ -0,0 +1,10 @@
+
+angular.module('demo').controller('anchoredNavCtrl',function($scope,$rootScope) {
+	//this is for functionality related to demo code
+
+});
+
+angular.module('demo').controller('anchoredNavPLayDemoCtrl',function($scope,$rootScope,$sce) {
+	//this is for functionality related to play demo code
+
+});
