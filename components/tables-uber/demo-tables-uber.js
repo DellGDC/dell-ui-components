2ef714ec41d28b9d547fb309fe3d55f2fdec78b4
Clint Batte <clint_batte@dell.com>
2015-09-09 12:34:51 -0500

    adding new component

---
+++ components/tables-uber/demo-tables-uber.js
@@ -0,0 +1,10 @@
+
+angular.module('demo').controller('tablesUberCtrl',function($scope,$rootScope) {
+	//this is for functionality related to demo code
+
+});
+
+angular.module('demo').controller('tablesUberPLayDemoCtrl',function($scope,$rootScope,$sce) {
+	//this is for functionality related to play demo code
+
+});
