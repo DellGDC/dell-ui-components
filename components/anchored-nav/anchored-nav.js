3978b5eb49132bf563b7ae87a2e8daabec46205b
Clint Batte <clint_batte@dell.com>
2015-09-14 22:16:37 -0500

    reset

--- components/anchored-nav/anchored-nav.js
+++ components/anchored-nav/anchored-nav.js
@@ -11,91 +11,21 @@
 
 angular.module('dellUiComponents')
 
-    .directive('spy', function($timeout) {
+    .directive('navSticky', function($timeout) {
         return {
             restrict: 'CA',
             link: function ($scope, $element, iAttrs, controller) {
-
-
 
                 var sticky = new Waypoint.Sticky({
                     element: $('.nav-sticky')[0]
                 });
 
-                $(document).ready(function(){
-
-
-                    // ========== scroll to section ===========
-
-                    //$('a[href^="#"]').on('click',function (e) {
-                    //    e.preventDefault();
-                    //
-                    //    var target = this.hash;
-                    //    var $target = $(target);
-                    //
-                    //    $('html, body').stop().animate({
-                    //        'scrollTop': $target.offset().top - 100
-                    //    }, 900, 'swing');
-                    //});
-
-                    // ========== /scroll to section ===========
-
-
-                    var target_id = iAttrs.target;
-
-                    var sections = $('section')
-                        , nav = $('nav')
-                        , nav_height = nav.outerHeight();
-
-                    $(window).on('scroll', function () {
-                        var cur_pos = $(this).scrollTop();
-
-                        sections.each(function() {
-                            var top = $(this).offset().top - nav_height,
-                                bottom = top + $(this).outerHeight();
-
-                            if (cur_pos >= top && cur_pos <= bottom) {
-                                nav.find('a').removeClass('active');
-                                sections.removeClass('active');
-
-                                $(this).addClass('active');
-                                nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-                            }
-                        });
-                    });
-
-                    nav.find('a').on('click', function () {
-                        var $el = $(this)
-                            , id = $el.attr('href');
-
-                        $('html, body').animate({
-                            scrollTop: $(id).offset().top - 100
-                        }, 500);
-
-                        return false;
-                    });
-                    });
-
-
-
-                //var target_id;
-                //
-                ////is there a target?
-                //if(iAttrs.target) {
-                //
-                //    target_id = iAttrs.target;
-                //
-                //
-                //}
-                //if(target_id) {
-                //    $(target_id).find('.nav a[href^=#]').on('click', function(e){
-                //        e.preventDefault();
-                //        $element.addClass('.active')
-                //    });
-                //}
-
-
-
+                var waypoint = new Waypoint({
+                    element: document.getElementById('basic-waypoint'),
+                    handler: function() {
+                        alert('Basic waypoint triggered')
+                    }
+                });
 
 
 
