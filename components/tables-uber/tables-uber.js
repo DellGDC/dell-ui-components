5a6efea29a23799ea07a482ea560e162004bf44c
Clint Batte <clint_batte@dell.com>
2015-09-17 11:34:19 -0500

    adding more DataTable extensions

--- components/tables-uber/tables-uber.js
+++ components/tables-uber/tables-uber.js
@@ -21,25 +21,39 @@ angular.module('dellUiComponents')
             link: function($scope, $element, iAttrs, controller ) {
 
                 $('.table-responsive-columns').DataTable( {
-                    "ajax": "components/tables-uber/dataColumn.json",
-                    "columns": [
-                        { "data": "Company_name" },
-                        { "data": "Solution_name" },
-                        { "data": "Solution_ID" },
-                        { "data": "Owner" },
-                        { "data": "Last_edited" },
-                        { "data": "List_price" },
-                        { "data": "Customer_number" },
-                        { "data": "Reference_number" },
-                        { "data": "Quote_number" }
-                    ],
-                    "pagingType": "simple",
-                    "language": {
-                        "paginate": {
-                            "next": "Next&nbsp;<span aria-hidden=\"true\" class=\"icon-ui-arrowright\"><\/span>",
-                            "previous": "<span aria-hidden=\"true\" class=\"icon-ui-arrowleft\"><\/span>&nbsp;Previous"
-                        }
-                    }
+                    //"ajax": "components/tables-uber/dataColumn.json",
+                    //"columns": [
+                    //    { "data": "Company_name" },
+                    //    { "data": "Solution_name" },
+                    //    { "data": "Solution_ID" },
+                    //    { "data": "Owner" },
+                    //    { "data": "Last_edited" },
+                    //    { "data": "List_price" },
+                    //    { "data": "Customer_number" },
+                    //    { "data": "Reference_number" },
+                    //    { "data": "Quote_number" }
+                    //],
+                    //"pagingType": "simple",
+                    //"language": {
+                    //    "paginate": {
+                    //        "next": "Next&nbsp;<span aria-hidden=\"true\" class=\"icon-ui-arrowright\"><\/span>",
+                    //        "previous": "<span aria-hidden=\"true\" class=\"icon-ui-arrowleft\"><\/span>&nbsp;Previous"
+                    //    }
+                    //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                 } );
 
 
