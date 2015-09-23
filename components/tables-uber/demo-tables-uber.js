20730cec2b2e7d46a2b74617a64085b97431d350
Bo Lora <bolora@hotmail.com>
2015-09-23 13:13:27 -0500

    added editable cells without ajax save function

--- components/tables-uber/demo-tables-uber.js
+++ components/tables-uber/demo-tables-uber.js
@@ -2,6 +2,32 @@
 angular.module('demo').controller('tablesUberCtrl',function($scope,$rootScope) {
 	//this is for functionality related to demo code
 
+	$scope.exampleComplexTableData = {
+                    "ajax": "components/tables-uber/dataColumn.json",
+                    "columns": [
+                        {
+                        	"data": "Company_name",
+                        	"sClass":"editable"
+                    	},
+                        {"data": "Solution_name"},
+                        {"data": "Solution_ID"},
+                        {"data": "Owner"},
+                        {"data": "Last_edited"},
+                        {"data": "List_price"},
+                        {"data": "Customer_number"},
+                        {"data": "Reference_number"},
+                        {"data": "Quote_number"}
+                    ],
+                    "pagingType": "simple",
+                    "language": {
+                        "paginate": {
+                            "next": "Next&nbsp;<span aria-hidden=\"true\" class=\"icon-ui-arrowright\"><\/span>",
+                            "previous": "<span aria-hidden=\"true\" class=\"icon-ui-arrowleft\"><\/span>&nbsp;Previous"
+                        }
+                    }
+
+                };
+
 });
 
 angular.module('demo').controller('tablesUberPLayDemoCtrl',function($scope,$rootScope,$sce) {
--- components/tables-uber/tables-uber.js
+++ components/tables-uber/tables-uber.js
@@ -20,30 +20,51 @@ angular.module('dellUiComponents')
             restrict: 'C',
             link: function($scope, $element, iAttrs, controller ) {
 
-
-                $('.table-responsive-columns').DataTable({
-                    "ajax": "components/tables-uber/dataColumn.json",
-                    "columns": [
-                        {"data": "Company_name"},
-                        {"data": "Solution_name"},
-                        {"data": "Solution_ID"},
-                        {"data": "Owner"},
-                        {"data": "Last_edited"},
-                        {"data": "List_price"},
-                        {"data": "Customer_number"},
-                        {"data": "Reference_number"},
-                        {"data": "Quote_number"}
-                    ],
-                    "pagingType": "simple",
-                    "language": {
-                        "paginate": {
-                            "next": "Next&nbsp;<span aria-hidden=\"true\" class=\"icon-ui-arrowright\"><\/span>",
-                            "previous": "<span aria-hidden=\"true\" class=\"icon-ui-arrowleft\"><\/span>&nbsp;Previous"
+                var tableData, table;
+                if(iAttrs.tableData) {
+                    tableData = JSON.parse(iAttrs.tableData);
+                } else {
+                    tableData = {
+                        "ajax": "components/tables-uber/dataColumn.json",
+                        "columns": [
+                            {"data": "Company_name"},
+                            {"data": "Solution_name"},
+                            {"data": "Solution_ID"},
+                            {"data": "Owner"},
+                            {"data": "Last_edited"},
+                            {"data": "List_price"},
+                            {"data": "Customer_number"},
+                            {"data": "Reference_number"},
+                            {"data": "Quote_number"}
+                        ],
+                        "pagingType": "simple",
+                        "language": {
+                            "paginate": {
+                                "next": "Next&nbsp;<span aria-hidden=\"true\" class=\"icon-ui-arrowright\"><\/span>",
+                                "previous": "<span aria-hidden=\"true\" class=\"icon-ui-arrowleft\"><\/span>&nbsp;Previous"
+                            }
                         }
-                    }
 
-                });
+                    };
+                }
 
+                table = $element.DataTable(tableData);
+
+                if($element.hasClass('table-editable')) {
+                    $timeout(function(){
+                        console.log("editable table here");
+                        $element.find('td.editable').attr("contenteditable",true);
+                        $element.find('td.editable').on('blur',function(e){
+                            var newData = $(e.currentTarget).text(), data = table.cell( this ).data();
+                            if(data !== newData) {
+                                console.log( 'You edited '+data+' and changed it to '+newData,table);
+                            }
+                            
+                        } );
+                    });
+
+
+                }
 
 
 
