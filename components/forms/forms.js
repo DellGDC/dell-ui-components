f1852c58b9b1679af689c02841c18e5e7de7eb2e
unknown <Timothy_Maloney@WN7X64-566NP32.aus.amer.dell.com>
2015-10-09 14:44:03 -0500

    Installed and adjust the UI and scripts for Dell UI

--- components/forms/forms.js
+++ components/forms/forms.js
@@ -1,4 +1,4 @@
-/**
+/*
  * Created by Clint_Batte on 3/24/2015.
  */
 angular.module('dellUiComponents')
@@ -534,5 +534,39 @@ return {
             $scope.emptyName = $attributes.emptyName || '*State';
         }
     };
-});
+})
 
+.controller('DatepickerDemoCtrl', function ($scope) {
+        $scope.today = function() {
+            $scope.dt = new Date();
+        };
+        // $scope.today();
+
+        $scope.clear = function () {
+            $scope.dt = null;
+        };
+
+        // Disable weekend selection
+        $scope.disabled = function(date, mode) {
+            return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+        };
+
+        $scope.toggleMin = function() {
+            $scope.minDate = $scope.minDate ? null : new Date();
+        };
+        // $scope.toggleMin();
+
+        $scope.open = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            $scope.opened = true;
+        };
+
+        $scope.dateOptions = {
+            formatYear: 'yy',
+            startingDay: 1
+        };
+
+        $("thead > tr > th").addClass("theader");
+    });
