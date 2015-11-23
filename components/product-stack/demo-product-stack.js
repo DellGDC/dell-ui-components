e142c8dfba1a24d35de17c78682ac2ae9c8fff02
Clint Batte <clint_batte@dell.com>
2015-11-23 15:22:24 -0600

    adding product stack

---
+++ components/product-stack/demo-product-stack.js
@@ -0,0 +1,10 @@
+
+angular.module('demo').controller('productStackCtrl',function($scope,$rootScope) {
+	//this is for functionality related to demo code
+
+});
+
+angular.module('demo').controller('productStackPLayDemoCtrl',function($scope,$rootScope,$sce) {
+	//this is for functionality related to play demo code
+
+});
