3cd29d7b0955978970f4aedf47c6e65f7aa168fa
Clint Batte <clint_batte@dell.com>
2015-12-09 12:36:28 -0600

    check-in compare button in demo code

--- components/product-stack/demo-product-stack.js
+++ components/product-stack/demo-product-stack.js
@@ -2,6 +2,16 @@
 angular.module('demo').controller('productStackCtrl',function($scope,$rootScope) {
 	//this is for functionality related to demo code
 
+    $('input[type="checkbox"]').on('click', function () {
+            if ($(this).is(':checked')) {
+                $(".compare-default").hide();
+                $(".compare-active").removeClass('hide').addClass('show');
+            } else {
+                $(".compare-default").show();
+                $(".compare-active").removeClass('show').addClass('hide');
+            }
+        });
+
 });
 
 angular.module('demo').controller('productStackPLayDemoCtrl',function($scope,$rootScope,$sce) {
