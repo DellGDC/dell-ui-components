2fedb7fdd653ba7c97edad98d043b7bddf1e2427
Clint Batte <clint_batte@dell.com>
2016-02-26 15:03:22 -0600

    Adding build files

--- dist/dell-ui-components/dell-ui-components.js
+++ dist/dell-ui-components/dell-ui-components.js
@@ -21530,6 +21530,109 @@ var KeyTable;
     'datatables'
   ], o) : 'object' === typeof exports ? o(require('jquery'), require('datatables')) : jQuery && !jQuery.fn.dataTable.ColReorder && o(jQuery, jQuery.fn.dataTable);
 }(window, document));
+/**
+ * Copyright (c) 2007-2015 Ariel Flesler - aflesler<a>gmail<d>com | http://flesler.blogspot.com
+ * Licensed under MIT
+ * @author Ariel Flesler
+ * @version 2.1.1
+ */
+;
+(function (f) {
+  'use strict';
+  'function' === typeof define && define.amd ? define(['jquery'], f) : 'undefined' !== typeof module && module.exports ? module.exports = f(require('jquery')) : f(jQuery);
+}(function ($) {
+  'use strict';
+  function n(a) {
+    return !a.nodeName || -1 !== $.inArray(a.nodeName.toLowerCase(), [
+      'iframe',
+      '#document',
+      'html',
+      'body'
+    ]);
+  }
+  function h(a) {
+    return $.isFunction(a) || $.isPlainObject(a) ? a : {
+      top: a,
+      left: a
+    };
+  }
+  var p = $.scrollTo = function (a, d, b) {
+      return $(window).scrollTo(a, d, b);
+    };
+  p.defaults = {
+    axis: 'xy',
+    duration: 0,
+    limit: !0
+  };
+  $.fn.scrollTo = function (a, d, b) {
+    'object' === typeof d && (b = d, d = 0);
+    'function' === typeof b && (b = { onAfter: b });
+    'max' === a && (a = 9000000000);
+    b = $.extend({}, p.defaults, b);
+    d = d || b.duration;
+    var u = b.queue && 1 < b.axis.length;
+    u && (d /= 2);
+    b.offset = h(b.offset);
+    b.over = h(b.over);
+    return this.each(function () {
+      function k(a) {
+        var k = $.extend({}, b, {
+            queue: !0,
+            duration: d,
+            complete: a && function () {
+              a.call(q, e, b);
+            }
+          });
+        r.animate(f, k);
+      }
+      if (null !== a) {
+        var l = n(this), q = l ? this.contentWindow || window : this, r = $(q), e = a, f = {}, t;
+        switch (typeof e) {
+        case 'number':
+        case 'string':
+          if (/^([+-]=?)?\d+(\.\d+)?(px|%)?$/.test(e)) {
+            e = h(e);
+            break;
+          }
+          e = l ? $(e) : $(e, q);
+          if (!e.length)
+            return;
+        case 'object':
+          if (e.is || e.style)
+            t = (e = $(e)).offset();
+        }
+        var v = $.isFunction(b.offset) && b.offset(q, e) || b.offset;
+        $.each(b.axis.split(''), function (a, c) {
+          var d = 'x' === c ? 'Left' : 'Top', m = d.toLowerCase(), g = 'scroll' + d, h = r[g](), n = p.max(q, c);
+          t ? (f[g] = t[m] + (l ? 0 : h - r.offset()[m]), b.margin && (f[g] -= parseInt(e.css('margin' + d), 10) || 0, f[g] -= parseInt(e.css('border' + d + 'Width'), 10) || 0), f[g] += v[m] || 0, b.over[m] && (f[g] += e['x' === c ? 'width' : 'height']() * b.over[m])) : (d = e[m], f[g] = d.slice && '%' === d.slice(-1) ? parseFloat(d) / 100 * n : d);
+          b.limit && /^\d+$/.test(f[g]) && (f[g] = 0 >= f[g] ? 0 : Math.min(f[g], n));
+          !a && 1 < b.axis.length && (h === f[g] ? f = {} : u && (k(b.onAfterFirst), f = {}));
+        });
+        k(b.onAfter);
+      }
+    });
+  };
+  p.max = function (a, d) {
+    var b = 'x' === d ? 'Width' : 'Height', h = 'scroll' + b;
+    if (!n(a))
+      return a[h] - $(a)[b.toLowerCase()]();
+    var b = 'client' + b, k = a.ownerDocument || a.document, l = k.documentElement, k = k.body;
+    return Math.max(l[h], k[h]) - Math.min(l[b], k[b]);
+  };
+  $.Tween.propHooks.scrollLeft = $.Tween.propHooks.scrollTop = {
+    get: function (a) {
+      return $(a.elem)[a.prop]();
+    },
+    set: function (a) {
+      var d = this.get(a);
+      if (a.options.interrupt && a._last && a._last !== d)
+        return $(a.elem).stop();
+      var b = Math.round(a.now);
+      d !== b && ($(a.elem)[a.prop](b), a._last = this.get(a));
+    }
+  };
+  return p;
+}));
 angular.module('dellUiComponents', []);
 angular.module('dellUiComponents').config(function () {
 }).run([
@@ -21888,34 +21991,48 @@ angular.module('dellUiComponents').config(function () {
 }(jQuery));
 angular.module('dellUiComponents').directive('toggle', [
   '$rootScope',
-  function ($rootScope) {
+  '$timeout',
+  '$compile',
+  function ($rootScope, $timeout, $compile) {
     return {
       restrict: 'A',
       link: function ($scope, $element, $attrs, controller) {
         switch ($attrs.toggle) {
         case 'popover':
-          var destroy = function () {
-            $('[data-toggle="popover"]').popover('destroy');
+          var hidePopover = function () {
+            $element.popover('hide');
+            $element.blur();
           };
           if ($attrs.trigger === 'hover') {
-            $element.mouseover(function (event) {
-              event.preventDefault();
-              destroy();
-              $(this).popover('show');
-            });
+            $element.popover({ trigger: 'hover' });
           } else {
             $element.popover({ trigger: 'manual' });
-            $element.click(function (event) {
-              event.preventDefault();
-              if ($(this).attr('aria-describedby')) {
-                destroy();
-              } else {
-                destroy();
-                $(this).popover('show');
-              }
-              $('[data-dismiss="popover"]').bind('click', function (event) {
-                event.preventDefault();
-                destroy();
+            $element.on('click', function () {
+              $element.popover('toggle');
+            });
+            $element.on('shown.bs.popover', function () {
+              $element.next().off('click');
+              $element.next().on('click', function () {
+                $element.focus();
+              });
+              $('[data-dismiss="popover"]').on('click', function () {
+                $timeout(function () {
+                  $element.blur();
+                }, 300);
+              });
+              $element.off('blur');
+              $element.on('blur', function () {
+                $timeout(function () {
+                  if (!$element.is(':focus')) {
+                    hidePopover();
+                  }
+                }, 500);
+              });
+              $compile($element.next().contents())($scope);
+            });
+            $element.on('hidden.bs.popover', function () {
+              $element.on('click', function () {
+                $element.popover('show');
               });
             });
           }
@@ -23601,3 +23718,14 @@ angular.module('dellUiComponents').directive('tableExpandableRow', [
     };
   }
 ]);
+angular.module('dellUiComponents').directive('scroll', function () {
+  return {
+    restrict: 'C',
+    link: function ($scope, $element, $attr, fn) {
+      $element.click(function (event) {
+        event.preventDefault();
+        $.scrollTo($attr.href, 300, { axis: 'y' });
+      });
+    }
+  };
+});
