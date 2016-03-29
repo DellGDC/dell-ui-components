c6f12d08ef32c8ec497ed3fd09edb9f9c39d6166
Clint Batte <clint_batte@dell.com>
2016-03-28 21:39:07 -0500

    new build with lazy load

--- dist/dell-ui-components/dell-ui-components.js
+++ dist/dell-ui-components/dell-ui-components.js
@@ -21891,6 +21891,74 @@ angular.module('dellUiComponents').config(function () {
   };
 }(jQuery));
 (function ($) {
+  $.dellUIloadMore = function (el, options) {
+    // To avoid scope issues, use 'base' instead of 'this'
+    // to reference this class from internal events and functions.
+    var base = this;
+    // Access to jQuery and DOM versions of element
+    base.$el = $(el);
+    base.el = el;
+    // Add a reverse reference to the DOM object
+    base.$el.data('dellUIloadMore', base);
+  };
+  $.dellUIloadMore.defaultOptions = {
+    lazyLoad: false,
+    scrollTarget: window,
+    fadeIn: true,
+    loadMoreButtonText: 'Load more',
+    loadMoreIncrement: 5
+  };
+  $.fn.dellUIloadMore = function (options) {
+    if (options) {
+      $.dellUIloadMore.defaultOptions = $.extend($.dellUIloadMore.defaultOptions, options);
+    }
+    return this.each(function () {
+      new $.dellUIloadMore(this);
+      var options = $.dellUIloadMore.defaultOptions, element = $(this), visibleCount = 0, items = element.find('li'), elementId = typeof $(this).attr('id') !== 'undefined' ? $(this).attr('id') : Math.random(1 + Math.random() * 100000000000), button = '<p><button id="load-more-button-' + elementId + '" rel="' + elementId + '" type="button" class="btn btn-block">' + options.loadMoreButtonText + '</button></p>', loadMore = function () {
+          visibleCount = visibleCount + options.loadMoreIncrement;
+          items = element.find('li');
+          items.each(function (index) {
+            if (index < visibleCount && $(items[index]).is(':hidden')) {
+              $(this).addClass('in');
+              if (index + 1 === items.length) {
+                $('#load-more-button-' + elementId).remove();
+              }
+            }
+          });
+        }, initPagination = function () {
+          if (element.hasClass('load-more-lazy')) {
+            options.lazyLoad = true;
+          }
+          if (options.fadeIn) {
+            items.addClass('fade');
+          }
+          loadMore();
+          if (!options.lazyLoad) {
+            element.after(button);
+            $('#load-more-button-' + elementId).click(function () {
+              loadMore();
+            });
+          } else {
+            if (options.scrollTarget === window) {
+              $(options.scrollTarget).scroll(function () {
+                if ($(options.scrollTarget).scrollTop() + $(options.scrollTarget).height() === $(document).height()) {
+                  loadMore();
+                }
+              });
+            } else {
+              $(options.scrollTarget).scroll(function () {
+                if ($(this).scrollTop() + $(this).height() === $(this)[0].scrollHeight) {
+                  loadMore();
+                }
+              });
+            }
+          }
+        };
+      initPagination();
+    });
+  };
+}(jQuery));
+(function ($) {
   $.dellUIcontentGallery = function (el) {
     // To avoid scope issues, use 'base' instead of 'this'
     // to reference this class from internal events and functions.
@@ -22907,6 +22975,20 @@ angular.module('dellUiComponents').directive('tapToLoad', function () {
         paged: function (page) {
         }
       });
+    }
+  };
+}).directive('loadMore', function () {
+  return {
+    restrict: 'C',
+    link: function ($scope, $element, attrs) {
+      var options = {
+          lazyLoad: typeof attrs.lazyLoad !== 'undefined' ? attrs.lazyLoad === 'true' : false,
+          scrollTarget: typeof attrs.scrollTarget !== 'undefined' ? attrs.scrollTarget : window,
+          fadeIn: typeof attrs.fadeIn !== 'undefined' ? attrs.fadeIn === 'true' : true,
+          loadMoreButtonText: typeof attrs.loadMoreButtonText !== 'undefined' ? attrs.loadMoreButtonText : 'Load more',
+          loadMoreIncrement: typeof attrs.loadMoreIncrement !== 'undefined' ? parseInt(attrs.loadMoreIncrement) : 5
+        };
+      $element.dellUIloadMore(options);
     }
   };
 });
