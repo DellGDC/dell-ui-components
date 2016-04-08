2ab5f094f73cbd85ce8391a1f1778452eaf71a71
Lora <Bo_Lora@amer.dell.com>
2016-04-08 16:05:33 -0500

    fixed spacing issues and displayed headline on all bps

--- components/banner/demo-banner.js
+++ components/banner/demo-banner.js
@@ -3,22 +3,22 @@ angular.module('demo').controller('bannerCtrl',function($scope,$rootScope,$timeo
 	//this is for functionality related to demo code
 	$scope.initOptions = function() {
 		$(".jumbotron a>p+p").hide();
-		$(".jumbotron h5").removeClass('visible-lg').hide();
+		$(".jumbotron h5").hide();
 
 		$('[name="bannerOptions"]').on('click',function(){
 			console.log($(this).val());
 			switch($(this).val()) {
 				case 'eventTitle': 
-				$(".jumbotron h5").addClass('visible-lg').show();
+				$(".jumbotron h5").show();
 				$(".jumbotron a>p+p").hide();
 				break;
 				case 'legal':
-				$(".jumbotron h5").removeClass('visible-lg').hide();
+				$(".jumbotron h5").hide();
 				$(".jumbotron a>p+p").show();	
 				break;
 				default:
 				$(".jumbotron a>p+p").hide();
-				$(".jumbotron h5").removeClass('visible-lg').hide();		
+				$(".jumbotron h5").hide();		
 			}
 		});
 
